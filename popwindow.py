246c872c0c77b31353b136e39f5d0c4cfd565b39
Medina <brymedina@csu.fullerton.edu>
2023-09-12 17:02:08 -0700

    Buttons updated

--- popwindow.py
+++ popwindow.py
@@ -1,4 +1,7 @@
+import sys
+#import contacts.py
 import tkinter as tk
+
 
 #title opening page when launched goes right
 window = tk.Tk()
@@ -18,15 +21,39 @@ height = 2000
 addContactButton = tk.Button(
     window,
     text = "Add Contact",
-    width = 150,
-    height = 55,
+    width = 30,
+    height = 30,
     background = "blue",
     foreground = "black"    
 )
 
+#button for delCont
+delContactButton = tk.Button(
+    window,
+    text = "Delete Contact",
+    width = 30,
+    height = 30,
+    background = "red",
+    foreground = "black"    
+)
+
+#button for modCont
+modContactButton = tk.Button(
+    window,
+    text = "Modify Contact",
+    width = 30,
+    height = 75,
+    background = "yellow",
+    foreground = "black"    
+)
+
+delContactButton.pack()
 addContactButton.pack()
+modContactButton.pack()
+delContactButton.flash()
+addContactButton.flash()
+modContactButton.flash()
 
 
-
-
+#launches/runs the windowke
 window.mainloop()
