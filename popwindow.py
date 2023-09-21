9f9025b6fe26cb6c0da066af216cd5167159911b
Medina <brymedina@csu.fullerton.edu>
2023-09-21 13:03:46 -0700

    Buttons updated

--- popwindow.py
+++ popwindow.py
@@ -1,4 +1,4 @@
-import sys
+#import sys
 #import contacts.py
 import tkinter as tk
 
@@ -15,14 +15,14 @@ background = "white",
 foreground = "black",
 width = 2000,
 height = 2000
-).place(x=25,y=50)
+)
 
 #buttons for functions will be going here
 addContactButton = tk.Button(
     window,
     text = "Add Contact",
     width = 30,
-    height = 30,
+    height = 20,
     background = "blue",
     foreground = "black"    
 )
@@ -32,7 +32,7 @@ delContactButton = tk.Button(
     window,
     text = "Delete Contact",
     width = 30,
-    height = 30,
+    height = 20,
     background = "red",
     foreground = "black"    
 )
@@ -42,18 +42,23 @@ modContactButton = tk.Button(
     window,
     text = "Modify Contact",
     width = 30,
-    height = 75,
+    height = 20,
     background = "yellow",
-    foreground = "black"    
+    foreground = "black"
 )
 
-delContactButton.pack()
-addContactButton.pack()
-modContactButton.pack()
-delContactButton.flash()
-addContactButton.flash()
-modContactButton.flash()
+#separator for contact list that will appear on the right hand side
+#window.add_separator()
+#TSeparator = tk.SEPARATOR(window)
+#TSeparator.config(orient = 'vertical')
+#TSeparator.place(relx=.5, rely=0, relwidth=1, relheight=1)
+#separator = tk.SEPARATOR(window, orient='vertical').pack(fill = Y, expand = TRUE)
+#separator.place(relx=.5, rely=0, relwidth=1, relheight = 1)
 
+#placements for the buttons to make them look more uniform
+delContactButton.place(x=0, y=0)
+modContactButton.place(x=210,y=0)
+addContactButton.place(x=410,y=0)
 
-#launches/runs the windowke
+#launches/runs the window
 window.mainloop()
