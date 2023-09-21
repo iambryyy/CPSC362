4bad978c7c6145b99548bfea049cac485f2ee83c
ctse55 <107425268+ctse55@users.noreply.github.com>
2023-09-21 12:22:27 -0700

    Update contact_App.py

--- contact_App.py
+++ contact_App.py
@@ -1,12 +1,20 @@
 import contacts
 import popwindow
 
-c= contacts.Contacts()
+c = contacts.contacts()
+
 
 #An introduction interface to allow user to pick what they want to do 
 while True:
     print("Welcome to MyContacts")
     print()
     print("Click who you want to Edit")
-    
-
+    #user interavtive input
+    while input == True:
+        #if click insert button
+            #Ask the user to input a contac they wish to add
+            #links with contacts.py to add to contact database 
+        #if click delete button 
+            #Ask the user to input the name and phonenumber of contact they want to delete
+            #links with contacts.py database and delete selected contact
+        #if click modify button 
