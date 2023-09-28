83efd280abe703b61e8fbe06eb9f938248747a14
knguyen7567 <70248924+knguyen7567@users.noreply.github.com>
2023-09-28 14:24:23 -0700

    Update txtnotif.py

--- txtnotif.py
+++ txtnotif.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 account_sid = 'AC53136afc84d35948bf29c8cd8c7c9984'
-auth_token = '2e9acbbdf904233ba29efa921e31fad2'
+auth_token = '8dfd31773f5f10941c7a4ea4df3c675f'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
