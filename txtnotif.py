93b6b87165543ba6eb8f93a853d09d8561de1d40
knguyen7567 <70248924+knguyen7567@users.noreply.github.com>
2023-09-28 14:30:22 -0700

    Added Twilio API for text message notification

--- txtnotif.py
+++ txtnotif.py
@@ -1,7 +1,8 @@
+#Importing Twilio
 from twilio.rest import Client
 
-account_sid = 'AC53136afc84d35948bf29c8cd8c7c9984' # DO NOT CHANGE THIS
-auth_token = '8dfd31773f5f10941c7a4ea4df3c675f'# DO NOT CHANGE THIS
+account_sid = 'AC53136afc84d35948bf29c8cd8c7c9984' # Account SID || DO NOT CHANGE THIS
+auth_token = '8dfd31773f5f10941c7a4ea4df3c675f'# Auth Token || DO NOT CHANGE THIS
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
